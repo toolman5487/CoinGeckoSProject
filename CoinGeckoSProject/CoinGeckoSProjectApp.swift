d619095d8550c2f6013046ea58a47557da96dcb5
Willy <willy548798@gmail.com>
2025-10-14 21:29:34 +0800

    Initial Commit

---
+++ CoinGeckoSProject/CoinGeckoSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGeckoSProjectApp.swift
+//  CoinGeckoSProject
+//
+//  Created by Willy Hsu on 2025/10/14.
+//
+
+import SwiftUI
+
+@main
+struct CoinGeckoSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
